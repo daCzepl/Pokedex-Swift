6d5add4d67d4453f854227a5417cee7af212ff24
Cedric Bauer <cedric@Cedrics-Mac.local>
2022-12-15 16:55:11 +0100

    Added members and type

--- Pokedex/Pokedex/Model.swift
+++ Pokedex/Pokedex/Model.swift
@@ -1,8 +1,50 @@
-//
-//  Model.swift
-//  Pokedex
-//
-//  Created by Cedric Bauer on 15.12.22.
-//
+struct Pokedex {
+    
+    private (set) var pokemons = [Pokemon]()
 
-import Foundation
+    struct Pokemon {
+        
+        static let BASE = "https://pokemon-go-api.github.io/pokemon-go-api/api/pokedex.json"
+        
+        let id: String
+        let formId: String
+        let dexNr: Int
+        let generation: Int
+        let names: Name
+        let stats: Stats
+        let primaryType: PokemonType
+    }
+    
+    
+    struct Name: Codable {
+        let English, German, French, Italian, Japanese, Korean, Spanish: String
+    }
+    
+    struct Stats: Codable{
+        let stamina, attack, defense: Int
+    }
+    struct PokemonType: Codable {
+        let type: TypeElement	
+        let names: Name
+    }
+    enum TypeElement: String, Codable{
+        case bug = "POKEMON_TYPE_BUG"
+        case dark = "POKEMON_TYPE_DARK"
+        case dragon = "POKEMON_TYPE_DRAGON"
+        case electric = "POKEMON_TYPE_ELECTRIC"
+        case fairy = "POKEMON_TYPE_FAIRY"
+        case fire = "POKEMON_TYPE_FIRE"
+        case fighting = "POKEMON_TYPE_FIGHTING"
+        case flying = "POKEMON_TYPE_FLYING"
+        case ghost = "POKEMON_TYPE_GHOST"
+        case grass = "POKEMON_TYPE_GRASS"
+        case ground = "POKEMON_TYPE_GROUND"
+        case ice = "POKEMON_TYPE_ICE"
+        case normal = "POKEMON_TYPE_NORMAL"
+        case poison = "POKEMON_TYPE_POISON"
+        case psychic = "POKEMON_TYPE_PSYCHIC"
+        case rock = "POKEMON_TYPE_ROCK"
+        case steel = "POKEMON_TYPE_STEEL"
+        case water = "POKEMON_TYPE_WATER"
+    }
+}
